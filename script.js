7f1f941f8f4e78a8e8b0f9830b587c8310a79a6e
Nodirbek <@Nodirbek>
2022-11-14 14:43:54 +0500

    script

---
+++ script.js
@@ -0,0 +1,14 @@
+var name = prompt("Ismingizni kiriting")
+console.log(" Mening ismim " + name);
+var age = +prompt(" Yoshingiz nechida? ")
+console.log(' Men ' + age + " yoshdaman ");
+var misoll = +prompt('Misol 1: 17 + 13=? ')
+console.log(' Misol 1: 17 + 13 = ' + (17 + 13) + " Sizning javob: " + misoll);
+var misoll = +prompt('Misol 2: 14 - 7 =? ')
+console.log(' Misol 2: 14 - 7 = ' + (14 - 7) + " Sizning javob: " + misoll);
+var misoll = +prompt('Misol 3: 25 * 4 =? ')
+console.log(' Misol 3: 25 * 4 = ' + 25 * 4 + " Sizning javob: " + misoll);
+var misoll = +prompt('Misol 4: 81 / 3 =? ')
+console.log(' Misol 4: 81 / 3 = ' + 81 / 3 + " Sizning javob: " + misoll);
+var end = prompt('Consolga kirib javoblarni taqqoslang')
+console.log(end);
