31c844a07a30ee34064e1e38d5b8af623887b4a9
Nodirbek <@Nodir>
2022-11-30 14:47:40 +0500

    JS4

--- script.js
+++ script.js
@@ -1,14 +1,57 @@
-var name = prompt("Ismingizni kiriting")
-console.log(" Mening ismim " + name);
-var age = +prompt(" Yoshingiz nechida? ")
-console.log(' Men ' + age + " yoshdaman ");
-var misoll = +prompt('Misol 1: 17 + 13=? ')
-console.log(' Misol 1: 17 + 13 = ' + (17 + 13) + " Sizning javob: " + misoll);
-var misoll = +prompt('Misol 2: 14 - 7 =? ')
-console.log(' Misol 2: 14 - 7 = ' + (14 - 7) + " Sizning javob: " + misoll);
-var misoll = +prompt('Misol 3: 25 * 4 =? ')
-console.log(' Misol 3: 25 * 4 = ' + 25 * 4 + " Sizning javob: " + misoll);
-var misoll = +prompt('Misol 4: 81 / 3 =? ')
-console.log(' Misol 4: 81 / 3 = ' + 81 / 3 + " Sizning javob: " + misoll);
-var end = prompt('Consolga kirib javoblarni taqqoslang')
-console.log(end);
+// var name = prompt("Ismingizni kiriting")
+// console.log(" Mening ismim " + name);
+// var age = +prompt(" Yoshingiz nechida? ")
+// console.log(' Men ' + age + " yoshdaman ");
+// var misoll = +prompt('Misol 1: 17 + 13=? ')
+// console.log(' Misol 1: 17 + 13 = ' + (17 + 13) + " Sizning javob: " + misoll);
+// var misoll = +prompt('Misol 2: 14 - 7 =? ')
+// console.log(' Misol 2: 14 - 7 = ' + (14 - 7) + " Sizning javob: " + misoll);
+// var misoll = +prompt('Misol 3: 25 * 4 =? ')
+// console.log(' Misol 3: 25 * 4 = ' + 25 * 4 + " Sizning javob: " + misoll);
+// var misoll = +prompt('Misol 4: 81 / 3 =? ')
+// console.log(' Misol 4: 81 / 3 = ' + 81 / 3 + " Sizning javob: " + misoll);
+// var end = prompt('Consolga kirib javoblarni taqqoslang')
+// console.log(end);
+const users =  {
+    1:{
+    name :  prompt("Ismingiz" , "Name" ) ,
+    age : +prompt("Yoshingiz" ,"Age")
+    },
+   2:{
+    name : prompt("Ismingiz" , "Name"),
+    age : +prompt("Yoshingiz","Age")
+    },
+   3:{
+    name : prompt("Ismingiz", "Name"),
+    age : +prompt("Yoshingiz","Age")
+    },
+   4:{
+    name : prompt("Ismingiz", "Name"),
+    age : +prompt("Yoshingiz","Age")
+},
+5:{
+    name : prompt("Ismingiz", "Name"),
+    age : +prompt("Yoshingiz","Age")
+},
+6:{
+    name : prompt("Ismingiz", "Name"),
+    age : +prompt("Yoshingiz","Age")
+},
+7:{
+    name : prompt("Ismingiz", "Name"),
+    age : +prompt("Yoshingiz","Age")
+},
+8:{
+    name : prompt("Ismingiz", "Name"),
+    age : +prompt("Yoshingiz","Age")
+},
+9:{
+    name : prompt("Ismingiz", "Name"),
+    age : +prompt("Yoshingiz","Age")
+},
+10:{
+    name : prompt("Ismingiz", "Name"),
+    age : +prompt("Yoshingiz","Age")
+}
+}
+console.log(users);
